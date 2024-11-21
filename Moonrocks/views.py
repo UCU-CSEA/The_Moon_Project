e8804b1712b42424f498e877f2475da47cf4a140
anzonathan <anzonathan@gmail.com>
2024-11-21 11:11:00 +0300

    Add: connection to cadets table and view that data in themoon

--- Moonrocks/views.py
+++ Moonrocks/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Cadets
 
 # Create your views here.
 
 def rocks(request):
-    return render(request, 'Moonrocks/themoon.html')
+    cadets = Cadets.objects.all()
+    template_data = {}
+    template_data['title'] = 'The Moon'
+    template_data['cadet'] =  cadets
+    return render(request, 'Moonrocks/themoon.html', {'template_data':template_data})
--- rocket/admin.py
+++ rocket/admin.py
@@ -1,5 +1,1 @@
 from django.contrib import admin
-from . models import Cadets
-
-# Register your models here.
-admin.site.register(Cadets)
